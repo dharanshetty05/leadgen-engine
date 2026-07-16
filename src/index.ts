02bab25941bc9248da2591f842e4bcc79266a8e3
dharanshetty05 <dharanshetty05@gmail.com>
2026-07-16 09:09:43 +0530

    feat: scraped ICP channel data using Apify

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
-import { Actor } from "apify";
 import dotenv from "dotenv";
+import { ApifyClient } from "apify-client";
 
 dotenv.config();
 
+const client = new ApifyClient({
+    token: process.env.APIFY_TOKEN,
+});
+
 async function main() {
-    await Actor.init();
+    console.log("Running actor...");
+    const run = await client.actor("scrapesage/youtube-scraper").call({
+        mode: "searchChannels",
+        searchTerms: [
+            "business",
+            "psychology"
+        ],
+        maxResultsPerQuery: 5,
+        includeVideos: false,
+        enrichCreatorContacts: false,
+        language: "en"
+    });
 
-    console.log("Hello");
+    console.log(run.defaultDatasetId);
 
-    await Actor.exit();
+    const { items } = await client
+        .dataset(run.defaultDatasetId!)
+        .listItems();
+
+    console.log(items);
 }
 
 main();
